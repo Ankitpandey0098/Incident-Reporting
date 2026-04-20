8448f5b0c1d886d1a8cbc63750b7ad7c235bbc92
Ankitpandey0098 <ankitpd308@gmail.com>
2026-04-20 15:20:04 +0530

    Fix allowed hosts

--- backend/incident_api/settings.py
+++ backend/incident_api/settings.py
@@ -141,10 +141,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "incident-reporting-rjwi.onrender.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
 CSRF_TRUSTED_ORIGINS = [
+    "https://incident-reporting-rjwi.onrender.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
