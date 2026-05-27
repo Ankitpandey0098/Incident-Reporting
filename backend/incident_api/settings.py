fea72f1a061bdc432a4bba7c3c1b0fcff32dcc42
Ankitpandey0098 <ankitpd308@gmail.com>
2026-05-27 11:21:14 +0530

    Added security feature

--- backend/incident_api/settings.py
+++ backend/incident_api/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
 ]
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get("CLOUD_NAME"),
+    'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
     'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
     'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET"),
 }
