d296c377121b8d0d9c149220293c6ad6922834bb
Ankitpandey0098 <ankitpd308@gmail.com>
2026-05-29 16:31:12 +0530

    change setting to real email sending

--- backend/incident_api/settings.py
+++ backend/incident_api/settings.py
@@ -178,21 +178,22 @@ TIME_ZONE = "Asia/Kolkata"  # or your server timezone
 
 
 # ================= EMAIL CONFIG =================
+# ================= EMAIL CONFIG =================
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
+
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_TIMEOUT = 30
 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-
-
-DEFAULT_FROM_EMAIL = 'Incident Platform <pandeyji7112@gmail.com>'
+EMAIL_TIMEOUT = 10
 
 BASE_URL = "https://incident-reporting-rjwi.onrender.com"
 
