9713ba9b38f9984b2f00112893fe7e2f846ec526
Ankitpandey0098 <ankitpd308@gmail.com>
2026-05-29 16:59:22 +0530

    Port changed

--- backend/incident_api/settings.py
+++ backend/incident_api/settings.py
@@ -183,10 +183,10 @@ TIME_ZONE = "Asia/Kolkata"  # or your server timezone
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
 
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
