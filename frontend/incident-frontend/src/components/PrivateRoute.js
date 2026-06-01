7afca516b018d9f286f126ec90acbd174f62f932
Ankitpandey0098 <ankitpd308@gmail.com>
2026-06-01 12:08:41 +0530

    Remove console log 2

--- frontend/incident-frontend/src/components/PrivateRoute.js
+++ frontend/incident-frontend/src/components/PrivateRoute.js
@@ -4,23 +4,15 @@ const PrivateRoute = ({ children, role }) => {
   const token = localStorage.getItem("access");
   const userRole = localStorage.getItem("role");
 
-  console.log("🛡️ PrivateRoute CHECK:");
-  console.log("➡️ Required role:", role);
-  console.log("➡️ Token exists:", !!token);
-  console.log("➡️ Stored role:", userRole);
-  console.log("➡️ Current path:", window.location.pathname);
-
   if (!token) {
-    console.log("❌ No token → redirect to login");
     return <Navigate to="/login" replace />;
   }
 
   if (role && userRole !== role) {
-    console.log("❌ Role mismatch → redirect to dashboard");
+    
     return <Navigate to="/dashboard" replace />;
   }
 
-  console.log("✅ Access granted");
   return children;
 };
 
